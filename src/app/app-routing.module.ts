0b1901633997f738153137d86a03087e60c461d5
afdezcl <adrifdez_@uma.es>
2019-07-08 14:24:17 +0200

    Base project blank created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'films', loadChildren: './pages/films/films.module#FilmsPageModule' },
+  { path: 'film-details', loadChildren: './pages/film-details/film-details.module#FilmDetailsPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
